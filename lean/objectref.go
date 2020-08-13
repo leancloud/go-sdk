c2838bdfcc33e70dd159eec819b45e66fc6d1490
Mason Zhang <soasurs@outlook.com>
2020-08-13 15:06:58 +0800

    feat: add __op atomic operation

--- lean/objectref.go
+++ lean/objectref.go
@@ -6,6 +6,22 @@ type ObjectRef struct {
 	ID    string
 }
 
+type OpType string
+
+const (
+	BitAnd         OpType = "BitAnd"
+	BitOr          OpType = "BitOr"
+	BitXor         OpType = "BitXor"
+	Increment      OpType = "Increment"
+	Decrement      OpType = "Decrement"
+	Add            OpType = "Add"
+	AddUnique      OpType = "AddUnique"
+	AddRelation    OpType = "AddRelation"
+	Remove         OpType = "Remove"
+	RemoveRelation OpType = "RemoveRelation"
+	Delete         OpType = "Delete"
+)
+
 func (r *ObjectRef) Get() (*Object, error) {
 	// TODO
 	return nil, nil
@@ -16,7 +32,7 @@ func (r *ObjectRef) Set(data interface{}) error {
 	return nil
 }
 
-func (r *ObjectRef) Update() error {
+func (r *ObjectRef) Update(data []map[string]interface{}) error {
 	// TODO
 	return nil
 }
@@ -24,4 +40,32 @@ func (r *ObjectRef) Update() error {
 func (r *ObjectRef) Delete() error {
 	// TODO
 	return nil
+}
+
+func Op(op OpType, data interface{}) map[string]interface{} {
+	opObject := make(map[string]interface{})
+	opObject["__op"] = op
+
+	switch op {
+	case Increment:
+	case Decrement:
+		opObject["amount"] = data
+		break
+	case BitAnd:
+	case BitOr:
+	case BitXor:
+		opObject["value"] = data
+	case Add:
+	case AddUnique:
+	case AddRelation:
+	case Remove:
+	case RemoveRelation:
+		opObject["objects"] = data
+		break
+	case Delete:
+		opObject["delete"] = data
+		break
+	}
+
+	return opObject
 }
