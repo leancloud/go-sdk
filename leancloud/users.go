e6a2f2d77a61dffee4c9f885ca39cb6b1db6c260
Mason Zhang <soasurs@outlook.com>
2020-11-25 17:41:51 +0800

    feat: users' verification & password reset

--- leancloud/users.go
+++ leancloud/users.go
@@ -31,6 +31,27 @@ func (ref *Users) LogIn(username, password string) (*User, error) {
 	return decodeUser(respJSON)
 }
 
+func (ref *Users) LogInByMobilePhoneNumber(number, smsCode string) (*User, error) {
+	path := "/1.1/login"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"mobilePhoneNumber": number,
+		"smsCode":           smsCode,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options)
+	if err != nil {
+		return nil, err
+	}
+
+	respJSON := make(map[string]interface{})
+	if err := json.Unmarshal(resp.Bytes(), &respJSON); err != nil {
+		return nil, err
+	}
+
+	return decodeUser(respJSON)
+}
+
 func (ref *Users) SignUp(username, password string) (*User, error) {
 	reqJSON := map[string]string{
 		"username": username,
@@ -46,6 +67,48 @@ func (ref *Users) SignUp(username, password string) (*User, error) {
 		return nil, fmt.Errorf("unexpected error when parse User from response: want type *User but %v", reflect.TypeOf(decodedUser))
 	}
 	return user, nil
+}
+
+func (ref *Users) SignUpByMobilePhone(number, smsCode string) (*User, error) {
+	path := "/1.1/usersByMobilePhone"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"mobilePhoneNumber": number,
+		"smsCode":           smsCode,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options)
+	if err != nil {
+		return nil, err
+	}
+
+	respJSON := make(map[string]interface{})
+	if err := json.Unmarshal(resp.Bytes(), &respJSON); err != nil {
+		return nil, err
+	}
+
+	decodedUser, err := decodeUser(respJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodedUser, nil
+}
+
+func (ref *Users) ResetPasswordBySMSCode(number, smsCode, password string, authOptions ...AuthOption) error {
+	path := "/1.1/resetPasswordBySmsCode/"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"password":          password,
+		"mobilePhoneNumber": number,
+	}
+
+	_, err := ref.c.request(ServiceAPI, MethodPost, fmt.Sprint(path, smsCode), options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Client) NewUserQuery() *UserQuery {
@@ -64,4 +127,95 @@ func (ref *Users) Become(sessionToken string) (*User, error) {
 	}
 
 	return decodeUser(respJSON)
+}
+
+func (ref *Users) RequestEmailVerify(email string, authOptions ...AuthOption) error {
+	path := "/1.1/requestEmailVerify"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"email": email,
+	}
+
+	_, err := ref.c.request(ServiceAPI, MethodPost, path, options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ref *Users) RequestMobilePhoneVerify(number string, authOptions ...AuthOption) error {
+	path := "/1.1/requestMobilePhoneVerify"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"mobilePhoneNumber": number,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	if string(resp.Bytes()) != "{}" {
+		return fmt.Errorf(string(resp.Bytes()))
+	}
+
+	return nil
+}
+
+func (ref *Users) RequestPasswordReset(email string, authOptions ...AuthOption) error {
+	path := "/1.1/requestPasswordReset"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"email": email,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	if string(resp.Bytes()) != "{}" {
+		return fmt.Errorf(string(resp.Bytes()))
+	}
+
+	return nil
+}
+
+func (ref *Users) RequestPasswordResetBySMSCode(number string, authOptions ...AuthOption) error {
+	path := "/1.1/requestPasswordResetBySmsCode"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"mobilePhoneNumber": number,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	if string(resp.Bytes()) != "{}" {
+		return fmt.Errorf(string(resp.Bytes()))
+	}
+
+	return nil
+}
+
+func (ref *Users) RequestLoginSMSCode(number string, authOptions ...AuthOption) error {
+	path := "/1.1/requestLoginSmsCode"
+	options := ref.c.getRequestOptions()
+	options.JSON = map[string]string{
+		"mobilePhoneNumber": number,
+	}
+
+	resp, err := ref.c.request(ServiceAPI, MethodPost, path, options, authOptions...)
+	if err != nil {
+		return err
+	}
+
+	if string(resp.Bytes()) != "{}" {
+		return fmt.Errorf(string(resp.Bytes()))
+	}
+
+	return nil
 }
