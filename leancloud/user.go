9928546be59fd676d843796b98c47114aa69a305
soasurs <zzhang@leancloud.rocks>
2021-03-01 12:30:11 +0800

    feat: add default fields

--- leancloud/user.go
+++ leancloud/user.go
@@ -1,6 +1,12 @@
 package leancloud
 
+// User is a local representation of a user persisted to the LeanCloud server.
 type User struct {
 	Object
-	SessionToken string `json:"sessionToken"`
+	SessionToken        string `json:"sessionToken"`
+	Username            string `json:"username"`
+	Email               string `json:"email"`
+	EmailVerified       bool   `json:"emailVerified"`
+	MobilePhoneNumber   string `json:"mobilePhoneNumber"`
+	MobilePhoneVerified bool   `json:"mobilePhoneVerified"`
 }
